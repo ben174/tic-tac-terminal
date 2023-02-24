fc7f3ff409c428b3e2dd1194b0aac69cbc963587
Ben Friedland <ben174@gmail.com>
2023-02-23 17:02:51 -0800

    add tests

--- board.py
+++ board.py
@@ -1,4 +1,8 @@
+import logging
 from enum import Enum
+
+
+logging.basicConfig(filename='game.log', level=logging.DEBUG, force=True)
 
 
 class BoardState(Enum):
@@ -9,7 +13,6 @@ class BoardState(Enum):
     PLAYING = 'Playing'
     FINISHED = 'Finished'
     CATS_GAME = 'Cat\'s Game'
-    INVALID = 'Invalid'
 
 
 class Player(Enum):
@@ -22,7 +25,6 @@ class Player(Enum):
     PLAYER_O = 'O'
 
 
-
 class Board:
     '''
     A class to hold the stateful Tic-Tac-Toe board, including the
@@ -32,6 +34,7 @@ class Board:
     '''
 
     def __init__(self):
+        logging.info('Game start')
         self.board = [[None]*3 for i in range(3)]
         self.current_player = Player.PLAYER_X
         self.state = BoardState.PLAYING
@@ -48,26 +51,49 @@ class Board:
 
         """
         win_count = 0
-        win_cell = None
+        spans = []
+
+        # check each row and column
         for i in range(3):
-            print('i', i)
             row = self.board[i]
-            print('row', row)
-            print(set(row))
-            print()
             col = self.board[0][i], self.board[1][i], self.board[2][i]
-            print('col', col)
-            print(set(col))
-            print()
-            print()
-            # if self.board[i][0]
+            logging.debug('Checking row ({}): {}'.format(i, row))
+            logging.debug('Checking col ({}): {}'.format(i, col))
+            spans.append(row)
+            spans.append(col)
 
-            # if self.board[i][0] ==
+        # check diagonal front and back
+        diag_f = self.board[0][0], self.board[1][1], self.board[2][2]
+        diag_b = self.board[0][2], self.board[1][1], self.board[2][0]
+        logging.debug('Checking diagonal (\\): {}'.format(diag_f))
+        logging.debug('Checking diagonal (/): {}'.format(diag_b))
+        spans.append(diag_f)
+        spans.append(diag_b)
 
-        #TODO: raise exception based on win_count, also set self.state = 'INVALID'
-        #TODO: if game is finished set self.state = 'FINISHED'
-        #TODO: check cats game ?
-        # if len(set(row)) == 2 for all rows and cols and diagonals
+        # do the check
+        clear_row_found = False
+        for span in spans:
+
+            # check if at least one span isn't blocked
+            if len(set(span)) == 1 or None in set(span):
+                clear_row_found = True
+
+            if len(set(span)) == 1 and None not in set(span):
+                print('WINNER WINNER chicken dinner.')
+                win_count += 1
+
+        if not clear_row_found:
+            logging.info('Cat\'s game')
+            return BoardState.CATS_GAME
+
+        # if win, ensure exactly one win
+        if win_count == 1:
+            logging.info('Found one win')
+            return BoardState.FINISHED
+        elif win_count > 1:
+            logging.error('Invalid board state (multiple wins)')
+            raise MultipleWinError()
+
         return BoardState.PLAYING
 
     def select_cell(self, row, column):
@@ -85,33 +111,36 @@ class Board:
         """
         current_val = self.board[row][column]
         if current_val is not None:
-            raise InvalidMoveException('Attempted to select a non-empty cell')
+            raise InvalidMoveError('Attempted to select a non-empty cell')
         self.board[row][column] = self.current_player
         self.state = self.check_state()
         if self.state == BoardState.PLAYING:
             # only toggle current_player if the self.state is still PLAYING
-            self.current_player = Player.PLAYER_O if self.current_player == Player.PLAYER_X else Player.PLAYER_X
+            self.current_player = (Player.PLAYER_O
+                                   if self.current_player == Player.PLAYER_X
+                                   else Player.PLAYER_X)
+            logging.debug('Setting player: %s', self.current_player.value)
 
-    def __str__(self):
+    def __repr__(self):
         ret = ''
         for row in self.board:
             for cell in row:
                 if not cell:
                     ret += '-'
                 else:
-                    ret += cell
+                    ret += cell.value
             ret += '\n'
         return ret
 
 
-class MultipleWinException(Exception):
+class MultipleWinError(Exception):
     """ An exception which is thrown when the board has invalid state containing
     more than one win.
 
     """
 
 
-class InvalidMoveException(Exception):
+class InvalidMoveError(Exception):
     """ An exception which is thrown when the an invalid move attempt is made
     (selecting an already-occupied square.
 
--- tests/test_board.py
+++ tests/test_board.py
@@ -1,75 +1,136 @@
-import unittest
+import pytest
 
-from board import Board, MultipleWinException, InvalidMoveException
+from board import Board, BoardState, Player
+from board import MultipleWinError, InvalidMoveError
 
 
 def test_single_cell_no_win():
-    '''
-    Ensures a win is not awarded if there is a row which contains
+    """ Ensures a win is not awarded if there is a row which contains
     an empty cell.
-    '''
+    """
     board = Board()
-    board.board[0][1] = 'X'
-    print()
-    print(board)
+    board.select_cell(0, 1)
+    assert board.state == BoardState.PLAYING
+
+
+def test_win():
+    """ Plays a standard game, ensuring the PLAYING state remains
+    until the game is finished.
+    """
+    board = Board()
+    # X: X - -
+    board.select_cell(0, 0)
+
+    # O: O - -
+    board.select_cell(1, 0)
+
+    # X: X X -
+    board.select_cell(0, 1)
+
+    # O: O O -
+    board.select_cell(1, 1)
+    assert board.state == BoardState.PLAYING
+
+    # X: X X X -- Finish Him!
+    board.select_cell(0, 2)
+    assert board.state == BoardState.FINISHED
+
+
+def test_player_toggle():
+    """ Ensures the current player is toggled after each move.
+    """
+    board = Board()
+    assert board.current_player == Player.PLAYER_X
+    board.select_cell(0, 0)
+    assert board.current_player == Player.PLAYER_O
+    board.select_cell(0, 1)
+    assert board.current_player == Player.PLAYER_X
 
 
 def test_mixed_row():
-    '''
-    Ensures a win is not awarded if a row contains an X and an O
-    '''
-    pass
+    """ Ensures a win is not awarded if a row contains an X and an O
+    """
+    board = Board()
+
+    # X: X - -
+    board.select_cell(0, 0)
+
+    # O: X O -
+    board.select_cell(0, 1)
+
+    # X: X O X
+    board.select_cell(0, 2)
+    assert board.state == BoardState.PLAYING
 
 
 def test_row_win():
-    '''
-    Ensures a win is awarded when a row (and only one row) is complete.
-    '''
-    #TODO: Test each row
-    pass
+    """ Ensures a win is awarded when a row (and only one row)
+    is complete.
+    """
+    for i in range(3):
+        board = Board()
+        board.board[i] = [Player.PLAYER_X] * 3
+        assert board.check_state() == BoardState.FINISHED
 
 
-def test_column_win():
-    '''
-    Ensures a win is awarded when a row (and only one row) is complete.
-    '''
-    #TODO: Test each column
-    pass
+def test_col_win():
+    """ Ensures a win is awarded when a column (and only one column)
+    is complete.
+    """
+    for i in range(3):
+        board = Board()
+        board.board[0][i], board.board[1][i], board.board[2][i] = [Player.PLAYER_O] * 3
+        assert board.check_state() == BoardState.FINISHED
 
 
 def test_diagonal_win():
-    '''
-    Ensures a win is awarded when a diagonal (or reverse-diagonal)
+    """ Ensures a win is awarded when a diagonal (or reverse-diagonal)
     is complete.
-    '''
-    #TODO: Test each diagonal front
-    #TODO: Test each diagonal back
-    pass
+    """
+    # Diagonal: \
+    board = Board()
+    for i in range(3):
+        board.board[i][i] = Player.PLAYER_O
+    assert board.check_state() == BoardState.FINISHED
+
+    # Diagonal: /
+    board = Board()
+    board.board[2][0], board.board[1][1], board.board[0][2] = [Player.PLAYER_O] * 3
+    assert board.check_state() == BoardState.FINISHED
 
 
 def test_cats_game():
-    '''
-    Ensures the BoardState is set to CATS_GAME when a unwinnable
+    """ Ensures the BoardState is set to CATS_GAME when a unwinnable
     board state is reached.
-    '''
-    pass
+    """
+    board = Board()
+
+    # Construct an unwinnable game:
+    # O X O
+    # X O -
+    # X O X
+    board.board[0] = Player.PLAYER_O, Player.PLAYER_X, Player.PLAYER_O
+    board.board[0] = Player.PLAYER_X, Player.PLAYER_O, None
+    board.board[1] = Player.PLAYER_X, Player.PLAYER_O, Player.PLAYER_X
+    assert board.check_state() == BoardState.CATS_GAME
 
 
 def test_multiple_win_error():
-    # with pytest.raises(MultipleWinException):
-    #     pass
-    return
+    """ Ensures a MultipleWinError exception is generated if the board
+    contains multiple wins.
+    """
+    with pytest.raises(MultipleWinError):
+        board = Board()
+        board.board[0] = [Player.PLAYER_X] * 3
+        board.board[1] = [Player.PLAYER_O] * 3
+        board.check_state()
 
 
-def test_finished_board_has_correct_winner():
-    '''
-    Ensures a winning board has the `current_player` set correctly.
-    '''
-    pass
-
-
-def test_select_cell_toggles_player():
-    '''
-    Ensures a using the `select_cell` method of the Board
-    '''
-    pass
+def test_invalid_move_error():
+    """ Ensures an InvalidMoveError exception is generated if an attempt
+    is made to move on an already-occupied square.
+    """
+    with pytest.raises(InvalidMoveError):
+        board = Board()
+        board.select_cell(0, 1)
+        board.select_cell(0, 1)
